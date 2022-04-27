39d854eda7c4c2e39843dccd787db01f4f5db452
nbalda <77226506+nbalda1011@users.noreply.github.com>
2022-04-27 16:10:31 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './component/Customer';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="gray_bg">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello react
-        </p>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    "id": 1,
+    'image': 'https://placeimg.com/64/64/any',
+    'userName': "nb",
+    'userBirthday': '951011',
+    'gender': "man",
+    'job': "developer"
+  },
+  {
+    "id": 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'userName': "nb01",
+    'userBirthday': '951011',
+    'gender': "ma",
+    'job': "student"
+  },
+  {
+    "id": 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'userName': "nb02",
+    'userBirthday': '951011',
+    'gender': "man",
+    'job': "goverment"
+  }
+]
+
+class App extends Component {
+  render(){
+    return (
+        <div>
+          {
+            customers.map(c => {
+              return<Customer
+                key={c.id}
+                id={c.id}
+                name={c.userName}
+                image={c.image}
+                birthday={c.userBirthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            })
+          }
+        </div>
+    );
+  }
 }
 
 export default App;
